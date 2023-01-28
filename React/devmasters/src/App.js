5e52255539bb53b939aadcc535a92ac68c28fded
AntrikshNarang <itsantriksh@gmail.com>
2023-01-28 20:36:04 +0530

    initial changes

--- React/devmasters/src/App.js
+++ React/devmasters/src/App.js
@@ -1,13 +1,34 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Authpage from './components/Authpage';
+import Footer from './components/Footer';
+import Header from './components/Header';
 import Navbar from './components/NavBar';
+//import reactdom
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from "react-router-dom";
+
 
 
 function App() {
   return (
     <div className="App">
       <>
+      {/* <Router> */}
+
+      {/* <Header/> */}
       <Navbar/>
+      {/* <div className="container"> */}
+      {/* <Routes> */}
+        {/* <Route path="/login" element={<Authpage/>}/> */}
+      {/* </Routes> */}
+      {/* </div> */}
+
+      <Footer/>
+      {/* </Router> */}
       </>
     </div>
   );
--- React/devmasters/src/components/NavBar.js
+++ React/devmasters/src/components/NavBar.js
@@ -1,4 +1,11 @@
-import React from 'react'
+import React from 'react';
+import './Navbar.css'
+// import link from react router dom 
+import { Link } from 'react-router-dom';
+
+require('react-dom');
+window.React2 = require('react');
+console.log(window.React1 === window.React2);
 
 export default function NavBar() {
   return (
@@ -14,12 +21,12 @@ export default function NavBar() {
             <span className="navbar-toggler-icon"></span>
         </button>
         <div className="collapse navbar-collapse" id="navbarNav">
-            <ul className="navbar-nav">
+            <ul className="navbar-nav ml-auto">
                 <li className="nav-item active">
-                    <a className="nav-link" href="/">Home <span className="sr-only">(current)</span></a>
+                    <a className="nav-link" to='/'>Login</a>
                 </li>
                 <li className="nav-item">
-                    <a className="nav-link" href="/">Features</a>
+                    <a className="nav-link" href="/">Sign Up</a>
                 </li>
                 <li className="nav-item">
                     <a className="nav-link" href="/">Pricing</a>
